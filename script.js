f1e997adff8fb07597bb6d1e15be942ba2cbecc1
awfsie <alysondu@cpp.edu>
2025-10-06 23:29:46 -0700

    final

---
+++ script.js
@@ -0,0 +1,32 @@
+const img1 = document.getElementById("img1");
+const audio1 = document.getElementById("audio1");
+img1.addEventListener("click",function() {
+  console.log("clicked");
+  img1.classList.add("fade-in");
+  audio1.play();
+});
+const img2 = document.getElementById("img2");
+img2.addEventListener("click",function() {
+  console.log("clicked2");
+  img2.classList.add("fade-in");
+  audio1.play();
+});
+const img3 = document.getElementById("img3");
+img3.addEventListener("click",function() {
+  console.log("clicked3");
+  img3.classList.add("fade-in");
+  audio1.play();
+});
+const img4 = document.getElementById("img4");
+img4.addEventListener("click",function() {
+  console.log("clicked4");
+  img4.classList.add("fade-in");
+  audio1.play();
+});
+const img5 = document.getElementById("img5");
+img5.addEventListener("click",function() {
+  console.log("clicked5");
+  img5.classList.add("fade-in");
+  audio1.play();
+});
+
